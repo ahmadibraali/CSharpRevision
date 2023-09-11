60f79917dc252ca0c3149ae683b7209ad4c1835b
Ahmedibr2020 <ahmed.ibr2020@gmail.com>
2023-09-11 17:13:37 +0300

    Working in A very Simple Project To Retreive A Piece of data Using ADO Model

---
+++ Working-with-ADO/Data/DataHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class DataHelper
+    {
+
+        static SqlConnection sqlConnection = new SqlConnection("Data Source=;Initial Catalog=;Integrated Security=;");
+
+        public static DataTable GetDepartements()
+        {
+            SqlCommand sqlCommand = new SqlCommand("Select Dname,Dnum,MGRSSN from Departments", sqlConnection);
+            //sqlCommand.Connection = sqlConnection;
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+
+        }
+
+        public static DataTable GetDepartementEmployees(int depID)
+        {
+            SqlCommand sqlCommand = new SqlCommand($"Select * from Employee where Dno={depID}", sqlConnection);
+            //sqlCommand.Connection = sqlConnection;
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+
+        }
+
+    }
+}
