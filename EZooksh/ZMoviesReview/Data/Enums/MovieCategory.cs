9539169f227a9d263820c7f3a69c4b675d4c0aeb
Ahmedibr2020 <ahmed.ibr2020@gmail.com>
2023-09-12 17:18:03 +0300

    Creating an ASP.Net Project in A MVC Model

---
+++ EZooksh/ZMoviesReview/Data/Enums/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace ZMoviesReview.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy = 2,
+        Drama = 4,
+        Documentery = 8
+    }
+}
---
+++ EZooksh/ZMoviesReview/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ZMoviesReview.Data.Enums;
+
+namespace ZMoviesReview.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ImageURL { get; set; }
+
+        public MovieCategory movieCategory { get; set; }
+
+
+        // Relationships 
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+    }
+}
