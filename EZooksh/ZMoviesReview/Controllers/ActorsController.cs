b20b1378f693cfa8b983c1f42315af38968b0d7a
Ahmedibr2020 <ahmed.ibr2020@gmail.com>
2023-09-13 23:18:31 +0300

    Adding Actors Controller

---
+++ EZooksh/ZMoviesReview/Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZMoviesReview.Data;
+
+namespace ZMoviesReview.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // Injection Of Actors data
+            var data  = _context.Actors.ToList();
+
+            return View();
+        }
+    }
+}
