f0d1fa8699fb3ae3d2fa4e0a738eb963a040cd6e
Ahmedibr2020 <ahmed.ibr2020@gmail.com>
2023-09-14 17:30:01 +0300

    Adding ProducersController and inject data to it

---
+++ EZooksh/ZMoviesReview/Controllers/ProducersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZMoviesReview.Data;
+
+namespace ZMoviesReview.Controllers
+{
+    public class ProducersConroller : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersConroller(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // Injection Of Producers data
+            var data = _context.Producers.ToList();
+
+            return View();
+        }
+    }
+}
