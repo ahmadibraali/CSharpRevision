d43c19d3d82189a4d6224bc2c7704292244a346c
Ahmedibr2020 <ahmed.ibr2020@gmail.com>
2023-09-16 09:55:24 +0300

    Adding Producers View

--- EZooksh/ZMoviesReview/Models/Actor.cs
+++ EZooksh/ZMoviesReview/Models/Actor.cs
@@ -7,7 +7,7 @@ namespace ZMoviesReview.Models
         [Key]
         public int Id { get; set; }
 
-        [Display(Name ="Profile Picture URL")]
+        [Display(Name ="Profile Picture")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Profile Name")]
         public string FullName { get; set;}
